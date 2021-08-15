fca8a3074ca70a4b19af079acf04cd49573912b5
Destoky <adestoky@gmail.com>
2021-08-15 12:02:08 +0200

    17. Les "props"

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-// import Item from "./Item.js";
+import Item from "./Item.js";
 
 function App() {
-  console.log("Chargement du composant " + Date.now());
+  console.log("chargement app");
   //Declaration
   const [age, setAge] = useState(24);
 
@@ -14,6 +14,9 @@ function App() {
       <h1>Coucou je suis l'App</h1>
       <h2>{age}</h2>
       <button onClick={changeAge}> +10 ans</button>
+      <hr />
+      <Item prenom="Alexandre" age={age}/>
+      <Item prenom="BENOIT"/>
     </div>
   );
 }
--- react-app/src/Item.js
+++ react-app/src/Item.js
@@ -1,6 +1,8 @@
-function Item() {
+function Item(props) {
+
+  console.log("item chargé " + props.prenom);
   return (
-    <h1>Salut je viens de l'item</h1>
+    <h1>Salut {props.prenom} je viens de l'item {props.age ? `et j'ai ${props.age} ans`:""}</h1>
   )
 }
 
