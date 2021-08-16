dd20c0e595c5c842b3efc83ae061f8f8ff9b64c1
Destoky <adestoky@gmail.com>
2021-08-16 11:47:47 +0200

    20. Utiliser des images

--- react-app/src/Components/Item/Item.js
+++ react-app/src/Components/Item/Item.js
@@ -1,9 +1,12 @@
 // import { useState } from "react";
 import "./Item.css";
+import {ReactComponent as Gorille} from "./biere.svg"
 function Item(props) {
   return (
     <div>
       <h1 className="titre-item">Salut je suis {props.prenom} et je viens de l'item</h1>
+      <img src={"/tomate.jpg"} alt="" />
+      <Gorille/>
     </div>
   );
 }
