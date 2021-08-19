3d2c9620a14740f527e76888dbd125101728b30b
Destoky <adestoky@gmail.com>
2021-08-19 23:20:25 +0200

    27. Afficher la liste

--- react-app/src/Components/Form/Form.js
+++ react-app/src/Components/Form/Form.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
 import Item from "../Item/Item.js";
 
-export default function() {
-  return(
+export default function () {
+  const [dataArr, setDataArr] = useState([{ txt: "Promener le chien" }, { txt: "Sport" }, { txt: "Coder avec React" }]);
+  return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
       <form className="mb-3">
-        <label htmlFor="todo" className="form-label mt-3" >Chose à faire</label>
-        <input type="text" className="form-control" id="todo"/>
+        <label htmlFor="todo" className="form-label mt-3">
+          Chose à faire
+        </label>
+        <input type="text" className="form-control" id="todo" />
         <button className="mt-2 btn btn-primary">Envoyez</button>
       </form>
       <h2>Liste des choses à faire :</h2>
       <ul className="list-group">
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
+        {dataArr.map((el, index) => {
+          return <Item txt={el.txt} key={index} />;
+        })}
       </ul>
     </div>
-  )
+  );
 }
--- react-app/src/Components/Item/Item.js
+++ react-app/src/Components/Item/Item.js
@@ -1,9 +1,11 @@
 
 
-export default function Item() {
+export default function Item(props) {
+  console.log(props);
+  console.log("charge");
   return (
     <li className="border justify-content-between align-items-center d-flex p-2 m-2">
-      <div className="p-3">Texte</div>
+      <div className="p-3">{props.txt}</div>
       <button className="btn btn-danger p-2 h-50">Supprimer</button>
     </li>
   );
