1b842af5a0908c9ee33b2c4058ee3a333aef686f
Destoky <adestoky@gmail.com>
2021-08-20 12:51:46 +0200

    32. Un appel à une API avec useEffect

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -2,22 +2,26 @@ import {useState,useEffect} from "react";
 import './App.css';
 
 function App() {
-  const [dataComponent, setDataComponent] = useState(1)
+
+  const [dataImg, setDataImg] = useState();
+  const [nbr, setNbr] = useState(0)
+  console.log("chargement");
 
   useEffect(() => {
-    console.log("chargement de App");
-  },[])
-  
-  const augmenter = () => setDataComponent(dataComponent + 1);
-  const diminuer = () => {
-    if(dataComponent > 0) setDataComponent(dataComponent - 1)
-  };
+   fetch("https://api.thecatapi.com/v1/images/search")
+   .then(response => response.json())
+   .then(data => {
+     console.log(data);
+     setDataImg(data)
+   })
+  },[nbr])
 
   return (
     <div className="App">
-      <p>Le state est : {dataComponent}</p>
-      <button onClick={augmenter}>Plus 1</button>
-      <button onClick={diminuer}>Moins 1</button>
+      <h2>{nbr}</h2>
+      <button onClick={()=>setNbr(nbr+ 1)}>Salut</button>
+      <hr />
+      {dataImg && <img src={dataImg[0].url} alt={dataImg[0].id} style={{width:"500px"}}/>}
     </div>
   );
 }
