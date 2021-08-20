67e957110bbbe84587ad162d9b5319ee7ed279a2
Destoky <adestoky@gmail.com>
2021-08-20 10:58:49 +0200

    29. Ajouter des éléments

--- react-app/src/Components/Form/Form.js
+++ react-app/src/Components/Form/Form.js
@@ -14,13 +14,22 @@ export default function () {
     setDataArr(filteredState);
   };
 
+  const addElement = e => {
+    e.preventDefault();
+    const addItem = (e.target.querySelector(".form-control").value);
+    const newList = [...dataArr,{txt:addItem,id:uuidv4()}];
+    setDataArr(newList);
+    e.target.querySelector(".form-control").value = "";
+    
+  }
+
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
-      <form className="mb-3">
+      <form className="mb-3" onSubmit={addElement}>
         <label htmlFor="todo" className="form-label mt-3">
           Chose à faire
         </label>
-        <input type="text" className="form-control" id="todo" />
+        <input type="text" className="form-control" id="todo"/>
         <button className="mt-2 btn btn-primary">Envoyez</button>
       </form>
       <h2>Liste des choses à faire :</h2>
