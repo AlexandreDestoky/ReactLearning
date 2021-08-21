75c1c6093efdf30f87ec9061a5263911979d8c11
Destoky <adestoky@gmail.com>
2021-08-21 09:48:22 +0200

    37. Sélection d'un tableau d'éléments

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -5,21 +5,30 @@ import Summer from "./Summer.mp4"
 function App() {
   const [toggle, setToggle] = useState(false);
 
-  const ref = useRef();
+  const ref = useRef([]);
+
+  const addToRef = el => {
+    console.log(el);
+    if(el && !ref.current.includes(el)) {
+      ref.current.push(el)
+    }
+  }
 
   useEffect(() => {
-    console.log(ref);
-    setTimeout(() => {
-      ref.current.pause()
-    }, 3000);
-  }, []);
-
+    console.log(ref.current[0]);
+  }, [])
   const toggleFunc = () => setToggle(!toggle);
 
   return (
     <div className="App">
 
-      <video ref={ref} width="750" height="500" autoPlay controls muted>
+      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
+        <source src={Summer} type="video/mp4"/>
+      </video>
+      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
+        <source src={Summer} type="video/mp4"/>
+      </video>
+      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
         <source src={Summer} type="video/mp4"/>
       </video>
       <button onClick={toggleFunc}>Toggle</button>
