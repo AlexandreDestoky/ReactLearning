6da82a8c6ece9f91a1dbe988a02f01ad16887b60
Destoky <adestoky@gmail.com>
2021-08-22 12:08:44 +0200

    46. Coder une navbar responsive

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Modal from "./Components/Modal/Modal";
 import Accord from "./Components/Accord/Accord";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <div className="App">
       {/* <Modal/> */}
-      <Accord/>
+      {/* <Accord/> */}
+      <Navbar/>
     </div>
   );
 }
---
+++ react-advanced/src/Components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+
+  const toggleNav = () => setToggleMenu(!toggleMenu);
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setLargeur(window.innerWidth);
+    }
+    window.addEventListener("resize",changeWidth);
+
+    return () => {
+      window.removeEventListener("resize",changeWidth);
+    }
+  }, [])
+
+  return (
+    <nav>
+      {(toggleMenu || largeur > 500) && (
+        <ul className="liste">
+          <li className="items">Accueil</li>
+          <li className="items">Services</li>
+          <li className="items">Contact</li>
+        </ul>
+      )}
+      <button onClick={toggleNav} className="btn">🍔</button>
+    </nav>
+  );
+}
