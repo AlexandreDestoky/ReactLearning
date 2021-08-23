843e3f678ee21f472366cc892f44e567add04553
Destoky <adestoky@gmail.com>
2021-08-23 11:55:42 +0200

    48. Utiliser un Router et un Switch

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Accueil from "./Components/Accueil";
+import Projet from "./Components/Projet";
+import Contact from "./Components/Contact";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Switch>
+        <Route path="/" exact component={Accueil}/>
+        <Route path="/Projet" exact component={Projet}/>
+        <Route path="/Contact" exact component={Contact}/>
+      </Switch>
+
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+{/* <Contact />
+<Projet />
+<Accueil /> */}
---
+++ react-router/src/Components/Projet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Projet() {
+  return (
+    <div>
+      <h1>Section Projet</h1>
+    </div>
+  )
+}
