ac6e2dcad5ceaee7db661e19a90d26dfb50aaea3
Destoky <adestoky@gmail.com>
2021-08-23 12:09:54 +0200

    49. Créer des liens et des navlinks

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -2,18 +2,19 @@ import "./App.css";
 import Accueil from "./Components/Accueil";
 import Projet from "./Components/Projet";
 import Contact from "./Components/Contact";
+import Nav from "./Components/Nav/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
-      <Switch>
-        <Route path="/" exact component={Accueil}/>
-        <Route path="/Projet" exact component={Projet}/>
-        <Route path="/Contact" exact component={Contact}/>
-      </Switch>
-
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Accueil} />
+          <Route path="/Projet" exact component={Projet} />
+          <Route path="/Contact" exact component={Contact} />
+        </Switch>
       </Router>
     </>
   );
@@ -21,6 +22,3 @@ function App() {
 
 export default App;
 
-{/* <Contact />
-<Projet />
-<Accueil /> */}
