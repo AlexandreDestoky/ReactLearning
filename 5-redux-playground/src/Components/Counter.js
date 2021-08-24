b0b488ce81ce73373a65108532dd47235a7fcdbc
Destoky <adestoky@gmail.com>
2021-08-24 11:55:45 +0200

    59. Créer un "Reducer"

--- 5-redux-playground/src/Components/Counter.js
+++ 5-redux-playground/src/Components/Counter.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 export default function Counter() {
+  const count = useSelector(state => state.count);
+  const dispatch = useDispatch();
 
-  const count = useSelector(state => state.count)
+  const incrFunc = () => {
+    dispatch({
+      type: "INCR",
+    });
+  };
+
+  const decrFunc = () => {
+    dispatch({
+      type: "DECR",
+    });
+  };
 
   return (
     <div>
       <h1>Le beau compteur</h1>
       <p>On est à : {count}</p>
+      <button onClick={incrFunc}>+ 1</button>
+      <button onClick={decrFunc}>- 1</button>
     </div>
-  )
+  );
 }
