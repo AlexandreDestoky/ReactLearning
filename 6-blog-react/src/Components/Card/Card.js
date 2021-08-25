9c87919233e59028d635fb9a744198e1a4899118
Destoky <adestoky@gmail.com>
2021-08-25 12:45:27 +0200

    67. Afficher les articles

--- 6-blog-react/src/Components/Card/Card.js
+++ 6-blog-react/src/Components/Card/Card.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 
 export default function Card(props) {
+  const rdmColor = () => {
+    const rdmRGB = () => Math.floor(Math.random() * 255);
+    return `rgb(${rdmRGB()},${rdmRGB()},${rdmRGB()})`;
+  };
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: rdmColor() }}>
       {props.children}
     </div>
-  )
+  );
 }
--- 6-blog-react/src/Containers/Home/Home.js
+++ 6-blog-react/src/Containers/Home/Home.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React from "react";
 import "./Home.css";
-import Card from '../../Components/Card/Card';
+import Card from "../../Components/Card/Card";
+import { getArticles } from "../../redux/articles/articleReducer";
+
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
+  const { articles } = useSelector(state => ({
+    ...state.articleReducer,
+  }));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (articles.length === 0) {
+      dispatch(getArticles());
+    }
+  }, []);
+
+
+
   return (
     <>
       <h1 className="home-title">Tous les articles</h1>
       <div className="container-cards">
-        <Card>
-          <h2>Hello Card</h2>
-        </Card>
+        {articles.map(item => {
+          return (
+            <Card key={uuidv4()}>
+              <h2>{item.title}</h2>
+              <a href="#">Lire l'article</a>
+            </Card>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
