4f25baa099e0aeae7acee8b6856cf513ad8eac21
Destoky <adestoky@gmail.com>
2021-08-26 12:17:28 +0200

    69. Créer le formulaire

---
+++ 6-blog-react/src/Components/Form/Form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Form.css";
+
+export default function Form() {
+  return (
+    <>
+      <h1 className="title-form">Écrivez un article</h1>
+      <form className="container-form">
+        <label htmlFor="title">Titre</label>
+        <input type="text" id="title" placeholder="Entrez votre nom" />
+
+        <label htmlFor="title">Votre Article</label>
+        <textarea id="article" placeholder="Votre Article ..."></textarea>
+
+        <button>Envoyez l'article</button>
+      </form>
+    </>
+  )
+}
