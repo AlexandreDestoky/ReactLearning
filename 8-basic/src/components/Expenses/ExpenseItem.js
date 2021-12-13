1d27bca32ea457d7e9f7153b1a54ba925df0cf0e
Destoky <adestoky@gmail.com>
2021-12-13 23:18:41 +0100

    Exercice 2

--- 8-basic/src/components/Expenses/ExpenseItem.js
+++ 8-basic/src/components/Expenses/ExpenseItem.js
@@ -15,7 +15,7 @@ export default function ExpenseItem(props) {
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">{props.amount}€</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card> 
--- 8-basic/src/components/Expenses/Expenses.js
+++ 8-basic/src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import Card from '../UI/Card'
-import "./Expenses.css"
-import ExpenseItem from './ExpenseItem';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpenseFilter";
 
 export default function Expenses(props) {
+  const [year, setYear] = useState();
+  const filterYear = expenses => {
+    return year ? expenses.filter(el => el.date.getFullYear() === year) : expenses;
+  };
 
   return (
     <Card className="expenses">
-      {props.items.map(el => <ExpenseItem {...el} key={el.id}/>)}
+      <ExpensesFilter changeYear={setYear} />
+      {filterYear(props.items).map(el => (
+        <ExpenseItem {...el} key={el.id} />
+      ))}
     </Card>
-  )
+  );
 }
