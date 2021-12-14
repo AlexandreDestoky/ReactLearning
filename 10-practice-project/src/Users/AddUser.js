de67dbbd3bde5c5c109f0aee4fd8f6d2897f0858
Destoky <adestoky@gmail.com>
2021-12-14 21:40:19 +0100

    93. Managing the User Input State

--- 10-practice-project/src/Users/AddUser.js
+++ 10-practice-project/src/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Bouton from "../UI/Bouton";
 import styles from "./AddUser.module.css";
@@ -6,14 +6,33 @@ import styles from "./AddUser.module.css";
 export default function AddUser() {
   const addUserHandler = e => {
     e.preventDefault();
+    if(!userNameInput || !ageInput) return;
+    console.log(userNameInput);
+    console.log(ageInput);
+    setUserNameInput("");
+    setAgeInput("");
   };
+
+  const [userNameInput, setUserNameInput] = useState("");
+  const [ageInput, setAgeInput] = useState("");
+
   return (
     <Card className={styles.input}>
-      <form onSubmit={addUserHandler} >
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" />
+        <input
+          type="text"
+          name="username"
+          onChange={e => setUserNameInput(e.target.value)}
+          value={userNameInput}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" name="age" />
+        <input
+          type="number"
+          name="age"
+          onChange={e => setAgeInput(e.target.value)}
+          value={ageInput}
+        />
         <Bouton>Click</Bouton>
       </form>
     </Card>
