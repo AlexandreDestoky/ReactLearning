e5f171058886d947585547443bf0f47c1a84fa3d
Destoky <adestoky@gmail.com>
2021-12-14 09:56:02 +0100

    68. Adding Conditional Return Statements

--- 8-basic/src/components/Expenses/ExpenseItem.js
+++ 8-basic/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,21 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-import "./ExpenseItem.css";
-
-export default function ExpenseItem(props) {
-  const clickHandler=() => {
-    setTitle("jambon");
-  }
-  const [title, setTitle] = useState(props.title)
+const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{props.amount}€</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card> 
-
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
+
+export default ExpenseItem;
--- 8-basic/src/components/Expenses/Expenses.js
+++ 8-basic/src/components/Expenses/Expenses.js
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
-import Card from "../UI/Card";
-import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
-import ExpensesFilter from "./ExpenseFilter";
+import React, { useState } from 'react';
 
-export default function Expenses(props) {
-  const [year, setYear] = useState();
-  const filterYear = expenses => {
-    return year ? expenses.filter(el => el.date.getFullYear() === year) : expenses;
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <Card className="expenses">
-      <ExpensesFilter changeYear={setYear} />
-      {filterYear(props.items).map(el => (
-        <ExpenseItem {...el} key={el.id} />
-      ))}
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
   );
-}
+};
+
+export default Expenses;
