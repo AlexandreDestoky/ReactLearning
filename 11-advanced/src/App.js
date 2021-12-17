6c17fdd6980f7173a49f5fc128539eab59600de3
Destoky <adestoky@gmail.com>
2021-12-17 12:41:50 +0100

    114. Using the UseEffect Cleanup Function

--- 11-advanced/src/App.js
+++ 11-advanced/src/App.js
@@ -1,40 +1,49 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+  //   if (storedUserLoggedInInformation === "1") {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
+
+  // const loginHandler = (email, password) => {
+  //   setIsLoggedIn(true);
+  //   localStorage.setItem("isLoggedIn", "1");
+  // };
+
+  // const logoutHandler = () => {
+  //   localStorage.removeItem("isLoggedIn");
+  //   setIsLoggedIn(false);
+  // };
+  const [prenom, setPrenom] = useState("Benoit")
+  let x = Date.now();
 
   useEffect(() => {
-
-      const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-  
-      if (storedUserLoggedInInformation === '1') {
-        setIsLoggedIn(true);
-      }
-  }, []);
-
-
-  const loginHandler = (email, password) => {
-    setIsLoggedIn(true);
-    localStorage.setItem("isLoggedIn", "1");
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn")
-    setIsLoggedIn(false);
-  };
-
+    let int = setInterval(() => {
+      console.log("salut" + x);
+      setPrenom("Andre" + x)
+    }, 3000);
+    return () => {
+      clearInterval(int);
+    }
+  }, [prenom])
 
   return (
     <>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      {/* <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      </main> */}
     </>
   );
 }
