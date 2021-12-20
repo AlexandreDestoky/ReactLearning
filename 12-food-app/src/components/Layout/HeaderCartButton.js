0b06cb74c244051e1d6c681bb32833a231592e72
Destoky <adestoky@gmail.com>
2021-12-20 14:49:44 +0100

    143. Using the Context

--- 12-food-app/src/components/Layout/HeaderCartButton.js
+++ 12-food-app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+
 export default function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+  console.log(cartCtx);
+
+  const nbrOfCartItems = cartCtx.items.reduce((a,b) => a + b.amount,0)
   return (
     <button className={classes.button} onClick={props.onclick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{nbrOfCartItems}</span>
     </button>
   );
 }
--- 12-food-app/src/store/cart-context.js
+++ 12-food-app/src/store/cart-context.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 
-const CartContext = React.createContext();
+export const CartContext = React.createContext();
 
 const defaultCartState = {
   items: [],
