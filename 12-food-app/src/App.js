1f6b3d5df0c43f6ae90f320285611efbf7ed0771
Destoky <adestoky@gmail.com>
2021-12-20 11:51:50 +0100

    136. Adding Individual Meal Items & Displaying Them

--- 12-food-app/src/App.js
+++ 12-food-app/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Layout/Header";
-import Meals from "./components/Layout/Meals/Meals";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
--- 12-food-app/src/components/Layout/HeaderCartButton.js
+++ 12-food-app/src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CartIcon from "./Cart/CartIcon";
+import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 export default function HeaderCartButton() {
   return (
