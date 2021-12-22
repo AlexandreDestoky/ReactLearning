38e15d2b2990bc24e676dbead0d2f07c41244f32
Destoky <adestoky@gmail.com>
2021-12-22 15:57:52 +0100

    178. Using async / await

--- 14-http/src/App.js
+++ 14-http/src/App.js
@@ -5,35 +5,35 @@ import "./App.css";
 
 function App() {
   const [dummyMovies, setDummyMovies] = useState([]);
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
-  const fetchMovies = () => {
-    fetch(`https://swapi.dev/api/films`)
-      .then(response => response.json())
-      .then(response => {
-        const films = response.results.map(movie => {
-          return {
-            id: movie.episode_id,
-            openingText: movie.opening_crawl,
-            title: movie.title,
-            releaseDate: movie.release_date,
-          };
-        });
-        setDummyMovies(films);
+
+  const fetchMovies = async () => {
+      const response = await fetch(`https://swapi.dev/api/films`);
+      const data = await response.json();
+      const films = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          openingText: movie.opening_crawl,
+          title: movie.title,
+          releaseDate: movie.release_date,
+        };
       });
+      setDummyMovies(films);
   };
+  // const fetchMovies = () => {
+  //   fetch(`https://swapi.dev/api/films`)
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       const films = response.results.map(movie => {
+  //         return {
+  //           id: movie.episode_id,
+  //           openingText: movie.opening_crawl,
+  //           title: movie.title,
+  //           releaseDate: movie.release_date,
+  //         };
+  //       });
+  //       setDummyMovies(films);
+  //     });
+  // };
 
   return (
     <>
