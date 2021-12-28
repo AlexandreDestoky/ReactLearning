87059a4769a55d8e8c18d7afec9d87b7ab724933
Destoky <adestoky@gmail.com>
2021-12-28 23:28:29 +0100

    205. Refactoring & Deriving States

--- 15-form/src/components/SimpleInput.js
+++ 15-form/src/components/SimpleInput.js
@@ -1,39 +1,33 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
-  const [isNameValid, setIsNameValid] = useState(false);
-  const [isTouched, setIsTouched] = useState(false)
+  const [isTouched, setIsTouched] = useState(false);
+
+  const isNameValid = enteredName.trim() !== "";
+  const error = !isNameValid && isTouched;
 
   const formSubmissionHandler = e => {
     e.preventDefault();
-    setIsTouched(true)
-    if (enteredName.trim() === "") {
-      setIsNameValid(false);
-      return;
-    }
-    setIsNameValid(true);
-
-    console.log(enteredName);
+    setIsTouched(true);
+    if (enteredName.trim() === "") return;
   };
 
-  // const inputName = useRef();
-  const changeInputName = (e) => {
-    setEnteredName(e.target.value)
-    if (enteredName.trim() !== "") {
-      setIsNameValid(true);
-    }
-  }
+  const changeInputName = e => {
+    setEnteredName(e.target.value);
+  };
+  
+  const changeIsTouched = () => {
+    setIsTouched(true);
+  };
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className={`form-control ${!isNameValid && isTouched? "invalid" : ""}`}>
+      <div className={`form-control ${error ? "invalid" : ""}`}>
         <label htmlFor="testing">Your Name</label>
-        <input type="text" id="testing" onChange={changeInputName} onBlur={() => setIsTouched(true)}/>
-        {!isNameValid && isTouched && <p className="error-text">Le nom n'est pas valide</p>}
+        <input type="text" id="testing" onChange={changeInputName} onBlur={changeIsTouched} />
+        {error && <p className="error-text">Le nom n'est pas valide</p>}
       </div>
-
-      <p>isNameValid : {JSON.stringify(isNameValid)}, isTouched : {JSON.stringify(isTouched)}</p>
 
       <div className="form-actions">
         <button>Submit</button>
