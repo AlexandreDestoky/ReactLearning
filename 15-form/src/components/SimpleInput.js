bb46cc85743bc57cdf0e0d132a6e918d6c4292d0
Destoky <adestoky@gmail.com>
2021-12-28 22:27:42 +0100

    202. Providing Validation Feedback

--- 15-form/src/components/SimpleInput.js
+++ 15-form/src/components/SimpleInput.js
@@ -1,25 +1,28 @@
 import { useRef, useState } from "react";
 
-
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
+  const [isNameValid, setIsNameValid] = useState(true);
 
-  const formSubmissionHandler = (e) => {
+  const formSubmissionHandler = e => {
     e.preventDefault();
-    if(enteredName.trim() === "") return;
+    if (enteredName.trim() === "") {
+      setIsNameValid(false);
+      return;
+    }
+    setIsNameValid(true);
+
     console.log(enteredName);
-    console.log("-----------");
-    console.log(inputName.current.value);
-  }
+  };
 
-  const inputName = useRef();
-
+  // const inputName = useRef();
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='testing'>Your Name</label>
-        <input type='text' id='testing' onChange={(e)=> setEnteredName(e.target.value)} ref={inputName} />
+      <div className={`form-control ${!isNameValid ? "invalid" : ""}`}>
+        <label htmlFor="testing">Your Name</label>
+        <input type="text" id="testing" onChange={e => setEnteredName(e.target.value)} />
+        {!isNameValid && <p className="error-text">Le nom n'est pas valide</p>}
       </div>
 
       <div className="form-actions">
