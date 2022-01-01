ee38a27a2f4a0620b461761ee234a7d89b072ac4
Destoky <adestoky@gmail.com>
2022-01-01 23:18:49 +0100

    Nettoyage Fichier

--- 13-testing/src/App.js
+++ 13-testing/src/App.js
@@ -7,16 +7,5 @@ export default function App() {
   const calcBodyFat = () => {
     return ((poids - masseMaigre) / poids) * 100;
   };
-  return (
-  //   <div>
-  //     <label htmlFor="">Poids</label>
-  //     <input
-  //       type="number"
-  //       placeholder="kg"
-  //       min={masseMaigre}
-  //       onChange={e => setPoids(e.target.value)}
-  //     />
-  //     <p>Pourcentage Bodyfat : {calcBodyFat().toFixed(2)}%</p>
-  //   </div>
-  // );
+  return <div></div>;
 }
