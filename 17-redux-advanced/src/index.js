37dc2090a3982478f410368203da6db464bfaae0
Destoky <adestoky@gmail.com>
2022-01-09 00:33:55 +0100

    252. Refresher / Practice: Part 1/2

--- 17-redux-advanced/src/index.js
+++ 17-redux-advanced/src/index.js
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom';
-
+import React from "react";
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import store from "./store/index"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
