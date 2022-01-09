db4823220787abc2746f68ca21547240891c42a3
Destoky <adestoky@gmail.com>
2022-01-09 18:31:27 +0100

    274. Using "Switch" and "exact" For Configuring Routes

--- 18-router/src/App.js
+++ 18-router/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -10,15 +10,18 @@ function App() {
     <div>
       <MainHeader />
       <main>
+      <Switch>
+
         <Route path='/welcome'>
           <Welcome />
         </Route>
-        <Route path='/products'>
+        <Route path='/products' exact>
           <Products />
         </Route>
-        <Route path='/product-detail/:productId'>
+        <Route path='/products/:productId'>
           <ProductDetail />
         </Route>
+      </Switch>
       </main>
     </div>
   );
