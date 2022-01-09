382cf5de4d8916a31f602eedc52240d4638c0457
Destoky <adestoky@gmail.com>
2022-01-09 19:49:01 +0100

    278. Practice Redirecting & Extracting Params

--- 18-router/src/pages/QuoteDetail.js
+++ 18-router/src/pages/QuoteDetail.js
@@ -1,14 +1,17 @@
-import { Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { Route, useParams } from 'react-router-dom';
+import Comments from "../components/comments/Comments"
 
 const QuoteDetail = () => {
   const params = useParams();
 
   return (
-    <Fragment>
+    <>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
-    </Fragment>
+      <Route path="/quotes/:quoteId/:comment">
+        <Comments/>
+      </Route>
+    </>
   );
 };
 
