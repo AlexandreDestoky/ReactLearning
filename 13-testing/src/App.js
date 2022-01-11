31d2f619d933ad113ed08e5685bc1cf03483d920
Destoky <adestoky@gmail.com>
2022-01-11 21:18:34 +0100

    282. Outputting Data on the "Details" Page

--- 13-testing/src/App.js
+++ 13-testing/src/App.js
@@ -1,11 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-  const masseMaigre = 60;
-  const [poids, setPoids] = useState(masseMaigre);
+  const [apiResult, setApiResult] = useState();
 
-  const calcBodyFat = () => {
-    return ((poids - masseMaigre) / poids) * 100;
+  const apiCall = () => {
+    fetchData("https://randomuser.me/api").then(response => setApiResult(response));
   };
-  return <div></div>;
+  return (
+    <div>
+      <h1>hi</h1>
+      <button onClick={apiCall}>FETCH</button>
+      <div>
+        {apiResult &&
+          apiResult.map((el, i) => {
+            return (
+              <div key={i}>
+                <p>
+                  {el.name.title} {el.name.first} {el.name.last}
+                </p>
+                <img
+                  src={el.picture.large}
+                  alt={`picture of ${el.name.first} ${el.name.last}`}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
+
+const fetchData = url => {
+  return fetch(url)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response.results);
+      return response.results;
+    });
+};
