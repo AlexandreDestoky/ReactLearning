1c0ffa4fbabc616e861ac4625cf3f0ea4dc8cfa7
Destoky <adestoky@gmail.com>
2022-01-14 23:36:49 +0100

    287. Getting Creative With Nested Routes

--- 18-router/src/components/quotes/QuoteList.js
+++ 18-router/src/components/quotes/QuoteList.js
@@ -3,6 +3,17 @@
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
   return (
     <>
